bbf5a67b96db401c722f785b6759247cdd621c0f
tranlamnhan88 <tranlamnhan8811@gmail.com>
2015-02-09 20:05:54 +0900

    finish exercises

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,31 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
+  def setup
+    @base_setup = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get :home
     assert_response :success
-    assert_select "title","Home | Ruby on rails tutorial sample app"
+    assert_select "title","Home | #{@base_setup}"
   end
 
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title","Help | Ruby on rails tutorial sample app"
+    assert_select "title","Help | #{@base_setup}"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on rails tutorial sample app"
+    assert_select "title", "About | #{@base_setup}"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | #{@base_setup}"
   end
 end
